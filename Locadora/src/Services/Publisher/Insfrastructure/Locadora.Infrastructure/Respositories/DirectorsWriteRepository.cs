cd9a16f565c74f5117ce22715a3d92739f4d5e97
Fernando <fernandobarrosdesak@gmail.com>
2025-03-12 16:35:01 -0300

    implementando repositorio de leitura de director e dvd

---
+++ Locadora/src/Services/Publisher/Insfrastructure/Locadora.Infrastructure/Respositories/DirectorsWriteRepository.cs
@@ -0,0 +1,45 @@
+﻿using Locadora.Application.Contracts;
+using Locadora.Domain.Entities;
+using Locadora.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locadora.Infrastructure.Respositories;
+
+public class DirectorsWriteRepository : IDirectorsWriteRepository
+{
+    private readonly LocadoraWriteContext _context;
+
+    public DirectorsWriteRepository(LocadoraWriteContext context)
+    {
+        _context = context;
+    }
+    public async Task<bool> Create(Director entity)
+    {
+        await _context.Directors.AddAsync(entity);
+
+        return await _context.SaveChangesAsync() > 0;
+    }
+    public async Task<bool> Update(Director entity)
+    {
+        _context.Directors.Update(entity);
+        
+        return await _context.SaveChangesAsync() > 0;
+    }
+    public async Task<bool> Delete(Guid id)
+    {
+        await _context.Directors.Where(d => d.Id == id).ExecuteDeleteAsync();
+
+        return await _context.SaveChangesAsync() > 0;
+    }
+    public async Task<Director> Get(Guid id)
+    {
+        return await _context.Directors.FindAsync(id);
+    }
+   public async Task<Director> GetDirectorWithMovies(Guid id)
+    {
+        return await _context.Directors.AsNoTracking()
+            .Include(d => d.Dvds)
+            .Where(d => d.Id == id)
+            .FirstOrDefaultAsync();
+    }
+}
---
+++ Locadora/src/Services/Publisher/Insfrastructure/Locadora.Infrastructure/Respositories/DvdWriteRepository.cs
@@ -0,0 +1,37 @@
+﻿using Locadora.Application.Contracts;
+using Locadora.Domain.Entities;
+using Locadora.Infrastructure.Context;
+
+namespace Locadora.Infrastructure.Respositories;
+
+public class DvdWriteRepository : IDvdsWriteRepository
+{
+    private readonly LocadoraWriteContext _context;
+
+    public DvdWriteRepository(LocadoraWriteContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Create(Dvd entity)
+    {
+        await _context.Dvds.AddAsync(entity);
+
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public Task<bool> Update(Dvd entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Dvd> Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
