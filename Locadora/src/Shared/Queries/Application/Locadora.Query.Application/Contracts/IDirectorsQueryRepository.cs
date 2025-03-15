9d15467b9ab0ffd7e836df969fa862eabd5f9312
Fernando <fernandobarrosdesak@gmail.com>
2025-03-14 22:07:51 -0300

    criacao das interfaces da camada de shared

---
+++ Locadora/src/Shared/Queries/Application/Locadora.Query.Application/Contracts/IDirectorsQueryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Locadora.Query.Domain.Models;
+
+namespace Locadora.Query.Application.Contracts;
+
+public interface IDirectorsQueryRepository : IQueryRepository<Director>
+{
+    Task<Director> GetByName(string name);
+}
---
+++ Locadora/src/Shared/Queries/Application/Locadora.Query.Application/Contracts/IDvdsQueryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Locadora.Query.Domain.Models;
+
+namespace Locadora.Query.Application.Contracts;
+
+public interface IDvdsQueryRepository : IQueryRepository<Dvd>
+{
+    
+}
